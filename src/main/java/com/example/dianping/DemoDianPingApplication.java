a0930f8bc3a82e644368ebf0ef411aea9ff047ab
YuLong <1003716039@qq.com>
2022-12-29 02:23:26 +0800

    Initial commit

---
+++ src/main/java/com/example/dianping/DemoDianPingApplication.java
@@ -0,0 +1,13 @@
+package com.example.dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDianPingApplication.class, args);
+    }
+
+}
