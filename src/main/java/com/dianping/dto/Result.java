bfe3125fac7c25405a9e23e7f49fc4c2285fccd0
YuLong <1003716039@qq.com>
2022-12-31 00:59:08 +0800

    dto层

---
+++ src/main/java/com/dianping/dto/Result.java
@@ -0,0 +1,33 @@
+package com.dianping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+    private Boolean success;
+    private String errorMsg;
+    private Object data;
+    private Long total;
+
+    public static Result ok() {
+        return new Result();
+    }
+
+    public static Result ok(Object data) {
+        return new Result();
+    }
+
+    public static Result ok(List<?> data, Long total) {
+        return new Result();
+    }
+
+    public static Result fail(String errorMsg) {
+        return new Result(false, errorMsg, null, null);
+    }
+}
