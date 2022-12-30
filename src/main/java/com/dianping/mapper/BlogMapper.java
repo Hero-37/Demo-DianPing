b8cac516753317b4e7aac190ac434aa07df40a06
YuLong <1003716039@qq.com>
2022-12-31 01:15:41 +0800

    mapper文件

---
+++ src/main/java/com/dianping/mapper/BlogMapper.java
@@ -0,0 +1,8 @@
+package com.dianping.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dianping.entity.Blog;
+
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
