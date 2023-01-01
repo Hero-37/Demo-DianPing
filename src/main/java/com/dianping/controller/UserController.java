fa76c8c91bf369df4b8fcb561373a3fb18d7d8d0
YuLong <1003716039@qq.com>
2023-01-02 01:24:28 +0800

    修改端口

--- src/main/java/com/dianping/controller/UserController.java
+++ src/main/java/com/dianping/controller/UserController.java
@@ -1,25 +1,37 @@
 package com.dianping.controller;
 
+import com.dianping.dto.LoginFormDTO;
 import com.dianping.dto.Result;
 import com.dianping.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Resource
     private IUserService userService;
 
+    /**
+     * 发送手机验证码
+     */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // 发送短信验证码并保存验证码
         return userService.sendCode(phone, session);
+    }
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
+        return userService.login(loginForm, session);
     }
 }
