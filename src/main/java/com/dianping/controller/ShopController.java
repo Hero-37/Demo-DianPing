676daa2bda70f022d48ea7beecbc171423e950ee
YuLong <1003716039@qq.com>
2023-07-29 20:14:37 +0800

    根据商铺名称查询商铺信息

--- src/main/java/com/dianping/controller/ShopController.java
+++ src/main/java/com/dianping/controller/ShopController.java
@@ -1,7 +1,11 @@
 package com.dianping.controller;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dianping.dto.Result;
+import com.dianping.entity.Shop;
 import com.dianping.service.IShopService;
+import com.dianping.utils.SystemConstants;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -37,5 +41,26 @@ public class ShopController {
             @RequestParam(value = "y", required = false) Double y
     ) {
         return shopService.queryShopByType(typeId, current, x, y);
+    }
+
+
+    /**
+     * 根据商铺名称关键字查询商铺信息
+     * @param name  商铺名称关键字
+     * @param current  页码
+     * @return  商铺列表
+     */
+    @GetMapping("/of/name")
+    public Result queryShopByName(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "current", defaultValue = "1") Integer current
+    ) {
+        // 根据类型分页查询
+        Page<Shop> page = shopService.query()
+                .like(StrUtil.isNotBlank(name), "name", name)
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+
+        // 返回数据
+        return Result.ok(page.getRecords());
     }
 }
